53332c3ecd1e330d51c411be9ecdb4994723feed
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-02-08 12:04:52 -0500

    Playwright Automation

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure'
+    trace: 'retain-on-failure',
+    screenshot: 'on-first-failure',
+    //video: 'retain-on-failure'
+    //video: {mode: 'retain-on-failure', size: {width: 1920, height: 1080} },
   },
 
   /* Configure projects for major browsers */
--- tests/day02/locators-intro.spec.js
+++ tests/day02/locators-intro.spec.js
@@ -1,12 +1,12 @@
 import {test} from "@playwright/test";
 
-test("Simple google test", async ({page}) => {
+test("Simple google test @wip", async ({page}) => {
     // test codes
     await page.goto("https://www.google.com");
 
     await page.waitForTimeout(3000);
 
-    let searchBox = page.locator("textarea[id='APjFqb']");
+    let searchBox = page.locator("textarea[id='APjFqb1234567890']");
 
     // await searchBox.type("CYDEO");
     await searchBox.fill("CYDEO");
