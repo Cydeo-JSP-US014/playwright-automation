d0efd6f5d9351a60f1f3c0bf451fa1a3166fcefb
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-02-15 11:16:25 -0500

    Playwright automation

---
+++ tests/reviews/CommonUI.js
@@ -0,0 +1,11 @@
+export class CommonUI {
+
+    static async login(page){
+        const encoded_credential = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
+
+        await page.setExtraHTTPHeaders( {'Authorization': `Basic ${encoded_credential}`} );
+
+        await page.goto(`${process.env.SEP_QA_URL}`);
+    }
+
+}
